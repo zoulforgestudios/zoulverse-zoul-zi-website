6e19dc7a2abd556dcfe92b22587216628c777d1f
zoulforge.studios <zoulforge.studios@gmail.com>
2025-11-15 13:12:06 +0530

    commited 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
-  import "tailwindcss";
 
   createRoot(document.getElementById("root")!).render(<App />);
   
